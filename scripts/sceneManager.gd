b79e12b500caf4ca2dabc9c7b5795c4801981b55
billy8thirty <kasparskoda@gmail.com>
2026-08-22 13:23:33 +0200

    added: test scene, main menu, test character

--- scripts/sceneManager.gd
+++ scripts/sceneManager.gd
@@ -4,15 +4,15 @@ extends Control
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
-func _on_startGame_pressed() -> void:
+func _on_start_Game_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level01.tscn")
-
 
 func _on_button_2_pressed() -> void:
 	pass # Replace with function body.
+
+func _on_test_scene_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/testScene.tscn")
